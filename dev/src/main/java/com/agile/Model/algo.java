4052043cb79ceb34b8963971415c14ef8c7ea181
avraux <antoine.vraux@insa-lyon.fr>
2022-11-29 01:59:04 +0100

    Parse Fonctionnel

--- dev/src/main/java/com/agile/Model/algo.java
+++ dev/src/main/java/com/agile/Model/algo.java
@@ -1,36 +1,72 @@
 package com.agile.Model;
 
 import java.io.File;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.List;  
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
    
 public class algo {  
+    /**
+     * @param args
+     */
     public static void main(String[] args) {  
-   
-     try {  
-   
-        File file = new File("smallMap.xml");  
-        JAXBContext jaxbContext = JAXBContext.newInstance(map.class);  
-   
-        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();  
-        map mapxml = (map) jaxbUnmarshaller.unmarshal(file);  
 
-        Warehouse ware = mapxml.getWarehouse(); 
-        System.out.println(ware.toString());  
-        List<Segment> listSeg=mapxml.getSegments();  
-        for(Segment seg:listSeg)  
-          System.out.println(seg.toString());  
+      try {
+        
+        //Get le fichier XML
 
-        List<Intersection> listInter = mapxml.getIntersections();
-        for(Intersection inter:listInter)  
-          System.out.println(inter.toString());  
-   
-      }catch (JAXBException e) {  
-        e.printStackTrace();  
+        File inputFile = new File("/Users/antoinevraux/Documents/INSA 4IF/PLD-agile/dev/src/main/java/com/agile/Model/smallMap.xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(inputFile);
+        doc.getDocumentElement().normalize();
+
+        //Get le Warehouse
+        NodeList listWarehouse = doc.getElementsByTagName("warehouse");
+        Node warehouse = listWarehouse.item(0);
+        Element eWarehouse = (Element) warehouse;
+        String wareAddress= eWarehouse.getAttribute("address");
+        Warehouse ware = new Warehouse(wareAddress);
+
+        //Get les intersections
+        List<Intersection> listInter = new ArrayList<Intersection>();
+        NodeList listIntersection = doc.getElementsByTagName("intersection");
+        for (int temp = 0; temp < listIntersection.getLength(); temp++) {
+          Node nInter = listIntersection.item(temp);
+          Element eInter = (Element) nInter;
+          String id = eInter.getAttribute("id");
+          String longitude = eInter.getAttribute("longitude");
+          String latitude = eInter.getAttribute("latitude");
+          Intersection inter = new Intersection(id, longitude, latitude);
+          listInter.add(inter);
+        } 
+
+        //Get les segments
+        List<Segment> listSeg = new ArrayList<Segment>();
+        NodeList listSegment = doc.getElementsByTagName("segment");
+        for (int temp = 0; temp < listSegment.getLength(); temp++) {
+          Node nSeg = listSegment.item(temp);
+          Element eSeg = (Element) nSeg;
+          String destination = eSeg.getAttribute("destination");
+          String length = eSeg.getAttribute("length");
+          String name = eSeg.getAttribute("name");
+          String origin = eSeg.getAttribute("origin");
+          Segment seg = new Segment(name, length, origin, destination);
+          listSeg.add(seg);
+        }
+
+        map xmlMap = new map(ware, listInter, listSeg);
+        System.out.println(xmlMap);
+         
+     } catch (Exception e) {
+        e.printStackTrace();
       }  
-   
-    }  
+    }
 }  
